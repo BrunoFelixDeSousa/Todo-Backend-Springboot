f94ac7f576de32c189586bf1d8e528dee97dce2d
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-11-08 21:16:21 -0300

    initial commit

---
+++ src/main/java/com/bruno/felix/todobackendspringboot/TodoBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.felix.todobackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBackendSpringbootApplication.class, args);
+    }
+
+}
