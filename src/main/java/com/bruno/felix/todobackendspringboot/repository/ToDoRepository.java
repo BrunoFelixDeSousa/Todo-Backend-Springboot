28564134c92d65b572c9e4090e7890bc16ec313d
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-11-08 23:14:17 -0300

    add ToDoRepository

---
+++ src/main/java/com/bruno/felix/todobackendspringboot/repository/ToDoRepository.java
@@ -0,0 +1,10 @@
+package com.bruno.felix.todobackendspringboot.repository;
+
+import com.bruno.felix.todobackendspringboot.model.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+}
